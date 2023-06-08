4eb57e395d5129564225527bbe4a1ee12f78756b
Trevor <wrecker1023@gmail.com>
2023-06-08 18:22:20 -0400

    added dotenv for sql info

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,10 @@
 // import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'aiden', {
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
